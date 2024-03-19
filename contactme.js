be8a094e420545cff09fff06f20eed86b36b1e7f
ManjulaKaaligi <manjulakaaligi5@gmail.com>
2024-03-19 17:24:26 +0530

    intialized the complete code

---
+++ contactme.js
@@ -0,0 +1,13 @@
+// function resetForm() {
+//     document.getElementById("name").value="";
+
+//     document.getElementById("email").value="";
+
+//     document.getElementById("mobileno").value="";
+
+//     document.getElementById("message").value="";
+// }
+
+function resetForm(){
+    document.getElementById("myForm").reset();
+}
